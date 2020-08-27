556955c682141e17d50af7a47a2e8afce132c06a
Xertor <marsp@abv.bg>
2020-08-27 04:08:15 +0300

    Entry Point

---
+++ Hazel_Engine/src/Hazel/EntryPoint.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argv)
+{
+	auto app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+
+	return 0;
+}
+
+#endif
