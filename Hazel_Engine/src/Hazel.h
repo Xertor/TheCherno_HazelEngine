fb42bc9224a9795f273c211a187db6fc5fb76bdb
Xertor <marsp@abv.bg>
2020-11-21 06:10:10 +0200

    Starting our 2D Renderer

--- Hazel_Engine/src/Hazel.h
+++ Hazel_Engine/src/Hazel.h
@@ -16,6 +16,7 @@
 
 // ---Renderer--------------------
 #include "Hazel/Renderer/Renderer.h"
+#include "Hazel/Renderer/Renderer2D.h"
 #include "Hazel/Renderer/RenderCommand.h"
 
 #include "Hazel/Renderer/Shader.h"
