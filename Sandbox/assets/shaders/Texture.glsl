28865daad1a30c7929c2a50bb2c05042f4567998
Xertor <marsp@abv.bg>
2020-11-25 04:16:17 +0200

    Single Shader 2D Renderer

--- Sandbox/assets/shaders/Texture.glsl
+++ Sandbox/assets/shaders/Texture.glsl
@@ -24,9 +24,10 @@ layout(location = 0) out vec4 color;
 
 in vec2 v_TextCoord;
 
+uniform vec4 u_Color;
 uniform sampler2D u_Texture;
 
 void main()
 {
-	color = texture(u_Texture, v_TextCoord * 10.0); //* vec4(1.0, 0.8, 0.8, 1.0); // red tint
+	color = texture(u_Texture, v_TextCoord * 10.0) * u_Color;
 }
